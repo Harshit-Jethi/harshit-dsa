bb426ebf5a7271c8fcfac8ca55d34df942c91bc2
Harshit-Jethi <harshitjethi8@gmail.com>
2024-06-27 18:37:12 +0530

    bool operator

--- basic.cpp
+++ basic.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std;
 int main () {
-
+/*
     cout<<"Hey harshit Jethi thi side"<<endl;
     int a =123;
     cout<<a<<endl;
@@ -15,4 +15,22 @@ int main () {
     cout<<d<<endl;
     int size =sizeof(a);
     cout<<"size of int:"<<size<<endl;
+    
+   int a='a';
+   cout<<a<<endl;
+   char ch=97;
+   cout<<ch<<endl;
+   char ch1=123456;
+   cout<<ch1<<endl;
+  unsigned a=-121;
+  cout<<a<<endl;
+  */
+ int a=3,b=4;
+ bool first=(a==b);
+ cout<<first<<endl;
+ bool second=(a>b);
+ cout<<second<<endl;
+ bool third=(a<b);
+ cout<<third<<endl;
+ 
 }
