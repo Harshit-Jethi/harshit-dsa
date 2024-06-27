3b22abbdfa2078fae7715c87ace47cb3d4bfcfff
Harshit-Jethi <harshitjethi8@gmail.com>
2024-06-27 17:48:54 +0530

    data tye & vaariable basic

--- basic.cpp
+++ basic.cpp
@@ -3,4 +3,16 @@ using namespace std;
 int main () {
 
     cout<<"Hey harshit Jethi thi side"<<endl;
+    int a =123;
+    cout<<a<<endl;
+    char b ='H';
+    cout<<b<<endl;
+    bool bl=true;
+    cout<<bl<<endl;
+    float f=1.2;
+    cout<<f<<endl;
+    double d=1.45;
+    cout<<d<<endl;
+    int size =sizeof(a);
+    cout<<"size of int:"<<size<<endl;
 }
