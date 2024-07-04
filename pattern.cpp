2c1dbb06ccd3fbd1dc2ef6af3e977a848b772602
Harshit-Jethi <harshitjethi8@gmail.com>
2024-07-04 23:08:10 +0530

    number pattern

--- pattern.cpp
+++ pattern.cpp
@@ -15,7 +15,21 @@ int main()
             j=j+1;
         }
                         cout<<endl;
-
         i=i+1;
     }
+
+/*int n;
+int i=1;
+cin>>n;
+while(i<=n)
+{
+    int j=1;
+    while(j<=n){
+    cout<<i;
+    j=j+1;
+}
+}
+cout<<endl;
+i=i+1;
+*/
 }
