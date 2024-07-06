3606ae4581754774ee242c23e109c95fe8b0f14b
Harshit-Jethi <harshitjethi8@gmail.com>
2024-07-06 19:38:56 +0530

    problem no 16

--- pattern.cpp
+++ pattern.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main()
 {
-    int n;
+  /*  int n;
     int i=1;
     cout<<"enter the number"<<endl;
     cin>>n;
@@ -17,8 +17,7 @@ int main()
                         cout<<endl;
         i=i+1;
     }
-
-/*int n;
+int n;
 int i=1;
 cin>>n;
 while(i<=n)
@@ -28,8 +27,212 @@ while(i<=n)
     cout<<i;
     j=j+1;
 }
+cout<<endl;
+i=i+1;
+}
+
+int n;
+int i=1;
+cin>>n;
+while(i<=n){
+    int j=1;
+    while(j<=n){
+        cout<<j;
+        j=j+1;
+    }
+   cout<<endl;
+   i=i+1;
+}
+
+int n;
+int i=1;
+cin>>n;
+while(i<=n){
+    int j=1;
+    while(j<=n){
+        cout<<n-j+1;
+        j=j+1;
+    }
+    cout<<endl;
+    i=i+1;
+}
+
+int n;
+int i=1;
+int count=1;
+cin>>n;
+while(i<=n){
+    int j=1;
+    while(j<=n){
+        cout<<count<<" ";
+        count=count+1;
+        j=j+1;
+    }
+            i=i+1;
+    cout<<endl;
+}
+
+int n;
+int i=1;
+cin>>n;
+while(i<=n)
+{
+    int j=1;
+    while(j<=i){
+        cout<<"*";
+        j=j+1;
+    }
+    cout<<endl;
+    i=i+1;
+}
+
+int n;
+int i=1;
+cin>>n;
+while(i<=n){
+    int j=1;
+    while(j<=i){
+        cout<<i;
+        j=j+1;
+    }
+    cout<<endl;
+    i=i+1;
+}
+
+int n;
+int i=1;
+int count=1;
+cin>>n;
+while(i<=n){
+    int j=1;
+    while(j<=i){
+        cout<<count<<" ";
+        count=count+1;
+        j=j+1;
+    }
+    cout<<endl;
+    i=i+1;
+}
+
+int n;
+int i=1;
+cin>>n;
+while(i<=n){
+    int j=1;
+    int value=i;
+    while(j<=i){
+        cout<<value;
+        value=value+1;
+        j=j+1;
+    }
+    cout<<endl;
+    i=i+1;
+}
+
+int n;
+int i=1;
+cin>>n;
+while(i<=n){
+    int j=1;
+    while(j<=i){
+    cout<<(i-j+1);
+    j=j+1;
 }
 cout<<endl;
 i=i+1;
+}
+//11
+int n;
+int i=1;
+cin>>n;
+while(i<=n){
+    int j=1;
+    while(j<=n){
+        char ch='A'+i-1;
+        cout<<ch;
+        j=j+1;
+    }
+    cout<<endl;
+    i=i+1;
+}
+//12
+int n;
+int i=1;
+char value='A';
+cin>>n;
+while(i<=n){
+    int j=1;
+    while(j<=n)
+    {
+        cout<<value;
+        value=value+1;
+        j=j+1;
+    }
+    cout<<endl;
+    i=i+1;
+}
+//13
+int n;
+int i=1;
+cin>>n;
+while(i<=n)
+{
+    int j=1;
+    while(j<=n){
+    char ch='A'+j-1;
+    cout<<ch;
+    j=j+1;
+}
+cout<<endl;
+i=i+1;
+}
+//14
+int n;
+int i=1;
+cin>>n;
+while(i<=n){
+    int j =1;
+    while(j<=n){
+        char ch ='A'+i+j-2;
+    cout<<ch;
+    j=j+1;
+    }
+    cout<<endl;
+    i=i+1;
+}
+//15
+int a;
+int i=1;
+cin>>a;
+while(i<=a){
+    int j=1;
+    char starting ='A'+a-1;
+    while(j<=i){
+        cout<<starting;
+        starting=starting+1;
+        j=j+1;
+    }
+    cout<<endl;
+    i=i+1;
+}
 */
+//16
+int a;
+int i=1;
+cin>>a;
+while(i<=a){
+    int space=a-i;
+    while(space){
+        cout<<" ";
+        space=space-1;
+    }
+        int j=1;
+        while(j<=i){
+            cout<<"*";
+            j=j+1;
+        }
+        cout<<endl;
+        i=i+1;
+    
+}
 }
